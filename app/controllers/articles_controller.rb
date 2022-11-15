bdc11ae874c7e0f24c650e529b4aca10278925c7
vrmcardoso <vrmcardoso@gmail.com>
2022-11-15 22:06:35 +0000

    Rails wikinimous done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,12 +4,43 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+
+    redirect_to articles_path
+  end
+
   def show
     @article = Article.find(params[:id])
   end
 
   def edit
     @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    render :edit
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "articles", to: "articles#index"
+  get "articles/new", to: "articles#new"
+  post "articles", to: "articles#create"
   get "articles/:id", to: "articles#show", as: :article
   get "articles/:id/edit", to: "articles#edit", as: :edit
+  patch "articles/:id", to: "articles#update"
+  delete "articles/:id", to: "articles#destroy", as: :destroy
 end
